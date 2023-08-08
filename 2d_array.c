ae24268e1097ae73fbf50127de8facc427fc4a5c
Himal Guragain <96119541+himalgnn@users.noreply.github.com>
2023-08-08 19:24:45 +0545

    Add 2d array program
    2d array program in C

---
+++ 2d_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int a[2][3],i,j;
+    printf("Enter the array elements\n");
+    for(i=0;i<2;i++){
+        for(j=0;j<3;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("Displaying array elements: \n");
+
+    for(i=0;i<2;i++){
+        for(j=0;j<3;j++){
+            printf("%d, ",a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
